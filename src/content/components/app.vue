cba1d364b866f6038a11f3a18ff91fdd9a82ea95
常红旭 <changhongxu@shizhuang-inc.com>
2021-11-26 20:02:04 +0800

    feat: 去除content

--- src/content/components/app.vue
+++ src/content/components/app.vue
@@ -2,16 +2,11 @@
  * @Author: migor
  * @Date: 2021-11-25 20:53:19
  * @LastEditors: migor
- * @LastEditTime: 2021-11-25 21:46:41
+ * @LastEditTime: 2021-11-26 20:01:43
  * @Description: file content
 -->
 <template>
-  <div class="content_page">
-    content_page
-    <div class="content_page_main">
-      content_page_main
-    </div>
-  </div>
+  <div class="content_page" />
 </template>
 
 <script>
@@ -21,15 +16,15 @@
 </script>
 
 <style lang="less" scoped>
-	.content_page{
-		color: red;
-		position: fixed;
-		z-index: 100001;
-		right: 10px;
-		bottom: 10px;
-		.content_page_main{
-			color: green;
-		}
-	}
+	// .content_page{
+	// 	color: red;
+	// 	position: fixed;
+	// 	z-index: 100001;
+	// 	right: 10px;
+	// 	bottom: 10px;
+	// 	.content_page_main{
+	// 		color: green;
+	// 	}
+	// }
 </style>
 
