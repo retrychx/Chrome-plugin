9d94c2aa66e0ebc44cd5ddbd9a2333ed945b9799
migor <migor@migordeMacBook-Pro.local>
2022-01-09 23:50:36 +0800

    feat: xiugai

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import AltihexDesignVue from "altihex-design"
-import 'altihex-design/dist/altihex.css'
-
-import app from './App'
-app.use(AltihexDesignVue)
-
-createApp(app).mount('#app')
